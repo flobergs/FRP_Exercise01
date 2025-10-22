f3753f49aca03eceaa2c46fea8c1f9e544ea9572
florian <florian.bergthaler@gmx.at>
2025-10-22 20:38:03 +0200

    task 2.1

--- src/main/scala/ex2_expr/Expr.scala
+++ src/main/scala/ex2_expr/Expr.scala
@@ -1,7 +1,37 @@
 package ex2_expr
 
 sealed trait Expr
-// TODO
+case class Lit(value: Double) extends Expr {
+
+  override def toString: String = value.toString
+
+//  // bonus
+//  override def toString: String = {
+//    val label = value.toString
+//
+//    if (label.endsWith(".0")) label.substring(0, label.length-".0".length)
+//    else label
+//  }
+}
+case class Var(name: String) extends Expr {
+  override def toString: String = name
+}
+
+sealed trait BinExpr(left: Expr, right: Expr) extends Expr
+case class Add(l: Expr, r: Expr) extends BinExpr(l, r) {
+  override def toString: String = s"($l+$r)"
+}
+case class Mul(l: Expr, r: Expr) extends BinExpr(l, r) {
+  override def toString: String = s"($l*$r)"
+}
+
+sealed trait UnyExpr(sub: Expr) extends Expr
+case class Neg(s: Expr) extends UnyExpr(s) {
+  override def toString: String = s"(-$s)"
+}
+case class Recip(s: Expr) extends UnyExpr(s) {
+  override def toString: String = s"(1/$s)"
+}
 
 def infix(expr: Expr): String = ??? // TODO:
 
@@ -9,3 +39,12 @@ def eval(expr: Expr, bds: Map[String, Double]): Double = ???
 
 def simplify(expr: Expr): Expr = ???
   
+def assertEquals(expr: Expr, expected: String): Unit = {
+  val actual: String = expr.toString
+  val success: Boolean = actual == expected
+  if (!success)
+  {
+    println(s"ERROR! actual \"$actual\" != expected \"$expected\"")
+    assert(success)
+  }
+}
--- src/main/scala/ex2_expr/ExprApp.scala
+++ src/main/scala/ex2_expr/ExprApp.scala
@@ -8,6 +8,21 @@ object ExprApp {
     println ("Hello World")
     //val e1 = Add(Lit(1), Min(Var("x")))
     //println(infix(e1))
+
+
+
+
+    println("asserting exercise tasks...")
+
+    assertEquals(Lit(123), "123.0")
+    assertEquals(Var("velocity"), "velocity")
+
+    assertEquals(Add(Lit(10), Var("x")), "(10.0+x)")
+    assertEquals(Mul(Lit(10), Var("x")), "(10.0*x)")
+    assertEquals(Neg(Lit(10)), "(-10.0)")
+    assertEquals(Recip(Lit(10)), "(1/10.0)")
+
+    println("...all assertions passed.")
   }
 
 }
