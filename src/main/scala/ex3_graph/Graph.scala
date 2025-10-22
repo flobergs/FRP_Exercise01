4db055e579c07ae500da275ec332a5a5323e7dcf
florian <florian.bergthaler@gmx.at>
2025-10-22 23:35:45 +0200

    task 3.3

--- src/main/scala/ex3_graph/Graph.scala
+++ src/main/scala/ex3_graph/Graph.scala
@@ -12,9 +12,42 @@ trait Graph {
     .filter((start, _) => start == node)
     .map((_, end) => end)
 
-  def computeDists(start: N): Map[N, Int] = ??? 
+  def computeDists(start: N): Map[N, Int] = {
+    def distsRec(queue: Queue[N], result: Map[N, Int], visited : Set[N]) : Map[N, Int] = {
+      if (queue.isEmpty) result
+      else {
+        val node = queue.head
+        val nextVisited = visited + node
+        val successorList = successors(node)
+          .removedAll(visited)
+          .removedAll(queue)
+        val nextQueue = queue.tail.appendedAll(successorList)
+        val nextResult = result ++ successorList.map(successor => (successor, result(node)+1))
+        distsRec(nextQueue, nextResult, nextVisited)
+      }
+    }
 
-  def computePaths(start: N): Map[N, List[N]] = ??? 
+    distsRec(Queue(start), Map(start -> 0), Set())
+  }
+
+  def computePaths(start: N): Map[N, List[N]] = {
+    def pathsRec(queue: Queue[N], result: Map[N, List[N]], visited : Set[N]) : Map[N, List[N]] = {
+      if (queue.isEmpty) result
+      else {
+        val node = queue.head
+        val nextVisited = visited + node
+        val successorList = successors(node)
+          .removedAll(visited)
+          .removedAll(queue)
+        val nextQueue = queue.tail.appendedAll(successorList)
+        val nextResult = result ++ successorList.map(successor => (successor, successor :: result(node)))
+        pathsRec(nextQueue, nextResult, nextVisited)
+      }
+    }
+
+    pathsRec(Queue(start), Map(start -> List()), Set())
+      .map((k,v) => (k, v.reverse))
+  }
 
   def computeValues[R](start: N, startValue: R, resultFn: (N, R) => R): Map[N, R] = ???
 
--- src/main/scala/ex3_graph/SimpleGraphExpl.scala
+++ src/main/scala/ex3_graph/SimpleGraphExpl.scala
@@ -8,16 +8,24 @@ object SimpleGraph extends IntGraph with App {
   println(s"edges= $edges")
   println(s"nodes= $nodes")
 
+  // Task 3.1
   assert(successors(1) == Set(2, 3))
   assert(successors(2) == Set(4))
   assert(successors(3) == Set(4))
 
+  // Task 3.2
   val dists = computeDists(1)
   println(dists)
+  assert(computeDists(1) == Map((1, 0), (2, 1), (3, 1), (4, 2)))
+  assert(computeDists(2) == Map((2, 0), (4, 1)))
 
+  // Task 3.3
   val paths = computePaths(1)
   println(paths)
+  assert(computePaths(1) == Map((1, List()), (2, List(2)), (3, List(3)), (4, List(2, 4))))
+  assert(computePaths(2) == Map((2, List()), (4, List(4))))
 
+  // Task 3.4 TODO
   val distsG = computeDistsG(1)
   println(distsG)
 
